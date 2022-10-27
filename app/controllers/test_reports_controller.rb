4f63850d60e445c6804c6bf1aa167f6b3bec85aa
T-Y89 <0809taikiyamashita@gmail.com>
2022-10-27 19:27:11 +0900

    create job_tree in /test_reports
    2
    
    done job_tree
    
    コメント記入
    
    メソッドをmodelに
    
    画面にsubeteno
    tdを表示
    
    完成
    
    treeの表示
    
    fix
    
    fix_for_M1 chip
    
    for M1
    
    一旦完成（ページネーションはまだ）
    
    一旦完成
    
    Co-authored-by: Naoto Kishino [LIFULL] <KishinoNaoto@lifull.com>
    
    ページネーション対応
    Co-authored-by: Naoto Kishino [LIFULL] <KishinoNaoto@lifull.com>
    
    完成
    
    rubicopの指摘対応
    
    fix speed
    
    Fix specified root_job_ids
    
    fix
    
    follow rubocop
    
    fix
    
    follow rails_best_practice
    
    move method to private
    
    Fix get children jobs
    
    fix
    
    create job_tree in /test_reports
    
    2
    
    done job_tree
    
    コメント記入
    
    メソッドをmodelに
    
    画面にsubeteno
    tdを表示
    
    完成
    
    treeの表示
    
    fix
    
    fix_for_M1 chip
    
    for M1
    
    一旦完成（ページネーションはまだ）
    
    一旦完成
    
    Co-authored-by: Naoto Kishino [LIFULL] <KishinoNaoto@lifull.com>
    
    ページネーション対応
    Co-authored-by: Naoto Kishino [LIFULL] <KishinoNaoto@lifull.com>
    
    完成
    
    rubicopの指摘対応
    
    fix speed
    
    Fix specified root_job_ids
    
    fix
    
    follow rubocop
    
    move method to private
    
    Fix get children jobs

--- app/controllers/test_reports_controller.rb
+++ app/controllers/test_reports_controller.rb
@@ -2,19 +2,19 @@
 
 class TestReportsController < ApplicationController
   before_action :check_round, only: %i[show update]
-
   def index
     per_page = 30
-    @jobs = Job.all
-               .page(params[:page])
-               .per(per_page)
-               .order('jobs.id DESC')
-    job_ids = Job.select('id').page(params[:page]).per(per_page).order('jobs.id DESC').to_a
-    @joined_jobs = Job.join_with_suites
-                      .select_group_concat_suites
-                      .group('jobs.id')
-                      .where(id: job_ids)
-                      .order('jobs.id DESC')
+    @page = Kaminari.paginate_array(Job.all_root_jobs.to_a, total_count: Job.all_root_jobs.length).page(params[:page]).per(per_page)
+    start_num = params[:page].nil? || params[:page] == 1 ? 0 : per_page * (params[:page].to_i - 1)
+    root_jobs = Job.root_jobs(start_num, per_page)
+    # 1ページで取得する小ジョブ数はper_page * 3件分で最低限取れると判断
+    children_jobs = Job.children_jobs(root_jobs.each{ |j| j.id }.min.id, per_page * 3)
+    root_job_tree = Job.create_job_tree(root_jobs, children_jobs)
+    children_job_tree = Job.create_job_tree(children_jobs, children_jobs)
+
+    @jobs = []
+    root_jobs.each { |job| child_loop(root_job_tree.merge(children_job_tree), job[:id], 0) }
+
     @test_case_result = TestCaseResult
     gon.controller_name = controller_name
     gon.action_name = action_name
@@ -22,9 +22,7 @@ class TestReportsController < ApplicationController
 
   def show
     respond_to do |format|
-      format.html do
-        set_var_for_show
-      end
+      format.html { set_var_for_show }
       format.js do
         check_update = TestCaseResult.get_failed_cases(params[:id], @round).check_update_in_ten_sec.ids
         if check_update.empty?
@@ -53,6 +51,33 @@ class TestReportsController < ApplicationController
   end
 
   private
+
+  def child_loop(job_tree, job_id, indent_num)
+    @jobs << job_tree[job_id]
+    @jobs.last[:indent_num] = indent_num
+    indent_num += 1
+    job_tree[job_id][:children].reverse_each { |child_job_id| child_loop(job_tree, child_job_id, indent_num) }
+  def create_job_tree(parent_jobs, children_jobs)
+    job_tree = {}
+    parent_jobs.each do |parent_job|
+      job_tree[parent_job.id] = {
+        id: parent_job.id,
+        job_start_time: parent_job.start_time,
+        command_and_option: parent_job.command_and_option,
+        device: parent_job.device,
+        service: parent_job.service,
+        category: parent_job.category,
+        total_time: parent_job.total_time,
+        children: []
+      }
+
+      children_jobs.each do |child_job|
+        job_tree[parent_job.id][:children] << child_job.id if parent_job.id == TestReport.get_parent(child_job.command_and_option)
+      end
+    end
+
+    job_tree
+  end
 
   def check_round
     set_job_id
@@ -83,10 +108,7 @@ class TestReportsController < ApplicationController
   end
 
   def set_var_for_show
-    @suite_data = Job.join_with_suites
-                     .select_group_concat_suites
-                     .group('jobs.id')
-                     .find(@job_id)
+    @suite_data = Job.join_with_suites([@job_id]).first
     set_var_for_render
     gon.passed_count = @data_for_test_reports[:stack_passed_counts]
     gon.failed_count = @data_for_test_reports[:failed_count]
